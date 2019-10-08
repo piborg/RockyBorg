3e36713528ca6b9884be63f9ce8b543375b35742
Arron Churchill <arron@frobotics.com>
2019-10-08 11:34:08 +0100

    Joystick script directly copied from Gamepad - needs imort fixes.

--- rbJoystick.py
+++ rbJoystick.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Load library functions we want
-import time
-import os
+# Load the libraries
 import sys
-import pygame
+import Gamepad
+import time
+sys.path.insert(0, "/home/pi/rockyborg")
 import RockyBorg
 
-# Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
-sys.stdout = sys.stderr
+# Settings for the gamepad
+gamepadType = Gamepad.PS4               # Class for the gamepad (e.g. Gamepad.PS3)
+joystickSpeed = 'LEFT-Y'                # Joystick axis to read for up / down position
+joystickSpeedInverted = True            # Set this to True if up and down appear to be swapped
+joystickSteering = 'RIGHT-X'            # Joystick axis to read for left / right position
+joystickSteeringInverted = False        # Set this to True if left and right appear to be swapped
+buttonSlow = 'L2'                       # Joystick button for driving slowly whilst held
+slowFactor = 0.5                        # Speed to slow to when the drive slowly button is held, e.g. 0.5 would be half speed
+buttonExit = 'PS'                       # Joystick button to end the program
+interval = 0.05                         # Time between motor updates in seconds, smaller responds faster but uses more processor time
 
-# Set up the RockyBorg
+# Power settings
+voltageIn = 1.2 * 8                     # Total battery voltage to the RockyBorg
+voltageOut = 6.0                        # Maximum motor voltage
+
+# Setup the power limits
+if voltageOut > voltageIn:
+    maxPower = 1.0
+else:
+    maxPower = voltageOut / float(voltageIn)
+
+# Setup the RockyBorg
 RB = RockyBorg.RockyBorg()
 #RB.i2cAddress = 0x21                  # Uncomment and change the value if you have changed the board address
 RB.Init()
@@ -23,7 +41,7 @@ if not RB.foundChip:
         print('No RockyBorg at address %02X, but we did find boards:' % (RB.i2cAddress))
         for board in boards:
             print('    %02X (%d)' % (board, board))
-        print('If you need to change the I2C address change the set-up line so it is correct, e.g.')
+        print('If you need to change the I²C address change the setup line so it is correct, e.g.')
         print('RB.i2cAddress = 0x%02X' % (boards[0]))
     sys.exit()
 
@@ -32,120 +50,82 @@ RB.SetCommsFailsafe(False)
 RB.MotorsOff()
 RB.SetMotorsEnabled(True)
 
-# Settings for the joystick
-axisUpDown = 1                          # Joystick axis to read for up / down position
-axisUpDownInverted = True               # Set this to True if up and down appear to be swapped
-axisLeftRight = 3                       # Joystick axis to read for left / right position
-axisLeftRightInverted = False           # Set this to True if left and right appear to be swapped
-buttonSlow = 6                          # Joystick button number for driving slowly whilst held (L2)
-slowFactor = 0.5                        # Speed to slow to when the drive slowly button is held, e.g. 0.5 would be half speed
-interval = 0.10                         # Time between updates in seconds, smaller responds faster but uses more processor time
+# Setup the state shared with callbacks
+global running
+running = True
 
-# Power settings
-voltageIn = 1.2 * 8                     # Total battery voltage to the RockyBorg
-voltageOut = 6.0                        # Maximum motor voltage
+# Create the callback functions
+def exitButtonPressed():
+    global running
+    print('EXIT')
+    running = False
 
-# Set up the power limits
-if voltageOut > voltageIn:
-    maxPower = 1.0
-else:
-    maxPower = voltageOut / float(voltageIn)
-
-# Set up pygame and wait for the joystick to become available
+# Wait for a connection
+RB.MotorsOff()
 RB.SetLed(True)
-os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
-pygame.init()
-#pygame.display.set_mode((1,1))
-print('Waiting for joystick... (press CTRL+C to abort)')
-while True:
-    try:
-        try:
-            pygame.joystick.init()
-            # Attempt to set up the joystick
-            if pygame.joystick.get_count() < 1:
-                pygame.joystick.quit()
-            else:
-                # We have a joystick, attempt to initialise it!
-                joystick = pygame.joystick.Joystick(0)
-                break
-        except pygame.error:
-            pygame.joystick.quit()
-    except KeyboardInterrupt:
-        # CTRL+C exit, give up
-        print('\nUser aborted')
-        RB.SetLed(False)
-        RB.SetCommsFailsafe(False)
-        sys.exit()
-    RB.SetLed(True)
-    time.sleep(0.2)
-    RB.SetLed(False)
+waitingToggle = True
+if not Gamepad.available():
+    print('Please connect your gamepad...')
+    while not Gamepad.available():
+        time.sleep(interval * 4)
+        waitingToggle = not waitingToggle
+        if waitingToggle:
+            RB.SetLed(False)
+        else:
+            RB.SetLed(True)
+print('Gamepad connected')
+gamepad = gamepadType()
 
-print('Joystick found')
-joystick.init()
+# Start the background updating
+gamepad.startBackgroundUpdates()
+RB.SetLed(True)
+
+# Register the callback functions
+gamepad.addButtonPressedHandler(buttonExit, exitButtonPressed)
+
+# Keep running while joystick updates are handled by the callbacks
 try:
-    RB.SetLed(False)
-    print('Press CTRL+C to quit')
-    driveLeft = 0.0
-    driveRight = 0.0
-    servoPosition = 0.0
-    running = True
-    hadEvent = False
-    upDown = 0.0
-    leftRight = 0.0
-    # Loop indefinitely
-    while running:
-        # Get the latest events from the system
-        hadEvent = False
-        events = pygame.event.get()
-        # Check if there are any events we need to respond to
-        for event in events:
-            if event.type == pygame.QUIT:
-                # User exit
-                running = False
-            elif event.type == pygame.JOYBUTTONDOWN:
-                # A button on the joystick just got pushed down
-                hadEvent = True
-                break
-            elif event.type == pygame.JOYAXISMOTION:
-                # A joystick has been moved
-                hadEvent = True
-                break
-        # We saw at least one change, update the outputs
-        if hadEvent:
-            # Read axis positions (-1 to +1)
-            if axisUpDownInverted:
-                upDown = -joystick.get_axis(axisUpDown)
-            else:
-                upDown = joystick.get_axis(axisUpDown)
-            if axisLeftRightInverted:
-                leftRight = -joystick.get_axis(axisLeftRight)
-            else:
-                leftRight = joystick.get_axis(axisLeftRight)
-            # Determine the drive power levels
-            driveLeft = upDown
-            driveRight = upDown
-            servoPos = leftRight
-            if leftRight < -0.05:
-                # Turning left
-                driveLeft *= 1.0 + (0.5 * leftRight)
-            elif leftRight > 0.05:
-                # Turning right
-                driveRight *= 1.0 - (0.5 * leftRight)
-            # Check for button presses
-            if joystick.get_button(buttonSlow):
-                driveLeft *= slowFactor
-                driveRight *= slowFactor
-            # Set the motors to the new speeds
-            RB.SetMotor1(-driveLeft * maxPower)
-            RB.SetMotor2(driveRight * maxPower)
-            RB.SetServoPosition(servoPos)
-        # Wait for the interval period
+    while running and gamepad.isConnected():
+        # Read the latest speed and steering
+        if joystickSpeedInverted:
+            speed = -gamepad.axis(joystickSpeed)
+        else:
+            speed = +gamepad.axis(joystickSpeed)
+        if joystickSteeringInverted:
+            steering = -gamepad.axis(joystickSteering)
+        else:
+            steering = +gamepad.axis(joystickSteering)
+
+        # Work out the adjusted speed
+        if gamepad.isPressed(buttonSlow):
+            finalSpeed = speed * slowFactor
+        else:
+            finalSpeed = speed
+
+        # Determine the drive power levels based on steering angle
+        servoPosition = steering
+        driveLeft = finalSpeed
+        driveRight = finalSpeed
+        if steering < -0.05:
+            # Turning left
+            driveLeft *= 1.0 + (0.5 * steering)
+        elif steering > +0.05:
+            # Turning right
+            driveRight *= 1.0 - (0.5 * steering)
+
+        # Set the motors to the new speeds and tilt the servo to steer
+        RB.SetMotor1(-driveLeft * maxPower)
+        RB.SetMotor2(driveRight * maxPower)
+        RB.SetServoPosition(servoPosition)
+
+        # Sleep for our motor change interval
         time.sleep(interval)
-    # Disable all drives
+finally:
+    # Ensure the background thread is always terminated when we are done
+    gamepad.disconnect()
+
+    # Turn the motors off
     RB.MotorsOff()
-except KeyboardInterrupt:
-    # CTRL+C exit, disable all drives
-    RB.MotorsOff()
-    RB.SetCommsFailsafe(False)
+
+    # Turn the LED off indicate we have finished
     RB.SetLed(False)
-print('')
